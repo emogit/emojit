9d0c3e3c48876838626f234cc30cb764b2f0c673
Justin D. Harris <juharris@users.noreply.github.com>
2021-01-24 00:28:52 -0500

    Support searching through the history.

--- app/scripts/components/History.tsx
+++ app/scripts/components/History.tsx
@@ -5,13 +5,15 @@ import Container from '@material-ui/core/Container'
 import Grid from '@material-ui/core/Grid'
 import Link from '@material-ui/core/Link'
 import { createStyles, Theme, WithStyles, withStyles } from '@material-ui/core/styles'
+import TextField from '@material-ui/core/TextField'
 import Typography from '@material-ui/core/Typography'
 import HistoryIcon from '@material-ui/icons/History'
-import React from 'react'
+import React, { ChangeEvent } from 'react'
 import { PageReactions } from '../api'
 import { getMessage } from '../i18n_helper'
 import { setupUserSettings } from '../user'
 import { progressSpinnerColor } from './constants'
+
 
 const styles = (theme: Theme) => createStyles({
 	historyIcon: {
@@ -22,6 +24,12 @@ const styles = (theme: Theme) => createStyles({
 	title: {
 		marginTop: theme.spacing(1.5),
 		marginBottom: theme.spacing(1),
+	},
+	search: {
+		marginTop: theme.spacing(1),
+		marginBottom: theme.spacing(1),
+		width: '100%',
+
 	},
 	center: {
 		display: 'flex',
@@ -40,28 +48,28 @@ const styles = (theme: Theme) => createStyles({
 	},
 	cartContent: {
 		padding: '12px !important',
-		// paddingBottom: '8px !important',
 	},
 })
 
 class History extends React.Component<WithStyles<typeof styles>, {
 	history: { pages: PageReactions[] } | undefined,
+	shownHistory: { pages: PageReactions[] } | undefined,
 	errorGettingHistory: string | undefined,
 }> {
 	constructor(props: any) {
 		super(props)
 		this.state = {
 			history: undefined,
+			shownHistory: undefined,
 			errorGettingHistory: undefined,
 		}
 	}
 
 	async componentDidMount() {
-
 		const { emojit } = await setupUserSettings()
 		try {
 			const history = await emojit.getAllUserReactions()
-			this.setState({ history })
+			this.setState({ history, shownHistory: history })
 		} catch (err) {
 			console.error(err)
 			const errorGettingHistory = getMessage('errorGettingHistory')
@@ -72,12 +80,32 @@ class History extends React.Component<WithStyles<typeof styles>, {
 	render(): React.ReactNode {
 		const { classes } = this.props
 
+		// TODO Support deleting individual items.
+
 		return <Container>
 			<Typography className={classes.title} component="h4" variant="h4">
 				<HistoryIcon className={classes.historyIcon} color="primary" fontSize="large" />
 				{getMessage('historyPageTitle') || "History"}
 			</Typography>
-			{this.state.history === undefined && this.state.errorGettingHistory === undefined && <div className={classes.center}>
+
+			{this.state.history && <TextField className={classes.search}
+				label="Enter a URL or emoji to search" variant="outlined"
+				onChange={(event: ChangeEvent<any>) => {
+					const text = (event.target.value || "").toLocaleLowerCase()
+					if (text) {
+						this.setState({
+							shownHistory: {
+								pages: this.state.history!.pages.filter(page => {
+									return page.pageUrl.toLocaleLowerCase().indexOf(text) > -1 || page.currentReactions.indexOf(text) > -1
+								})
+							}
+						})
+					} else {
+						this.setState({ shownHistory: this.state.history })
+					}
+				}}
+			/>}
+			{this.state.shownHistory === undefined && this.state.errorGettingHistory === undefined && <div className={classes.center}>
 				<CircularProgress size={70} style={{ color: progressSpinnerColor }}
 				/>
 			</div>}
@@ -88,7 +116,7 @@ class History extends React.Component<WithStyles<typeof styles>, {
 			</div>}
 
 			<Grid container className={classes.historyGrid} spacing={1}>
-				{this.state.history !== undefined && this.state.history.pages.map((page, index) =>
+				{this.state.shownHistory !== undefined && this.state.shownHistory.pages.map((page, index) =>
 					<Grid key={`page-${index}`} item xs={12}>
 						<Card className={classes.card} raised={true}>
 							<CardContent className={classes.cartContent}>
