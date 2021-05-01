d7b2e58474a5730a2e60395708d293dc39055183
Justin D. Harris <juharris@users.noreply.github.com>
2021-05-01 00:19:55 -0400

    Add a test.

--- app/scripts/user.ts
+++ app/scripts/user.ts
@@ -3,7 +3,7 @@ import { browser } from 'webextension-polyfill-ts'
 import { DEFAULT_SERVICE_URL, EmojitApi } from './api'
 
 // From https://stackoverflow.com/a/2117523/1226799
-function uuidv4() {
+export function uuidv4() {
 	return (1e7.toString() + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, cString => {
 		const c = parseInt(cString, 10)
 		return (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
