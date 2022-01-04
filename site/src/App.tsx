3046ce76857f1e9fad240baa65752ed5504d06b4
Justin D. Harris <juharris@users.noreply.github.com>
2022-01-03 22:51:23 -0500

    Start the widget.

--- site/src/App.tsx
+++ site/src/App.tsx
@@ -1,26 +1,20 @@
 import React from 'react'
-import logo from '../public/logo128.png'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import Home from './components/Home'
+import Layout from './components/Layout'
+import Widget from './components/Widget'
 
 function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-          Edit <code>src/App.tsx</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-          Learn React
-				</a>
-			</header>
-		</div>
-	)
+	return (<BrowserRouter>
+		<Routes>
+			<Route path="/widget" element={<Widget />} />
+			<Route path="/" element={<Layout />}>
+				<Route index element={<Home />} />
+				{/* <Route path="*" element={<NoPage />} /> */}
+			</Route>
+		</Routes>
+	</BrowserRouter>)
 }
 
 export default App
---
+++ site/src/components/Widget.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Widget() {
+	return (<div>
+		<h3>Emojit</h3>
+		{/* TODO */}
+	</div>)
+}
+
+export default Widget
