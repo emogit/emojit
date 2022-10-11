1d5e8964a708f407d26f6a735f6298bfa1ae595e
Justin H <juharris@users.noreply.github.com>
2022-10-10 23:15:29 -0400

    Get userId from localStorage.

--- site/src/components/Widget.tsx
+++ site/src/components/Widget.tsx
@@ -1,11 +1,16 @@
-import { EmojitClient, getMessage } from '@emogit/emojit-core'
+import { createNewUserId, EmojitClient, getMessage } from '@emogit/emojit-core'
 import { ReactionsComponent } from '@emogit/emojit-react-core'
 import React from 'react'
 
 function Widget() {
 	const currentUrl = document.baseURI
 	console.debug("currentUrl", currentUrl)
-	const userId = 'TODO'
+	const userIdKey = 'userId'
+	let userId = localStorage.getItem(userIdKey)
+	if (!userId) {
+		userId = createNewUserId()
+		localStorage.setItem('userId', userId)
+	}
 	const emojit = new EmojitClient(userId)
 	return (<ReactionsComponent
 		emojitClient={emojit}
